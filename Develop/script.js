eb1ff51c14b9cd71808f9a6de7904ae0c182115d
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-01-10 11:06:42 -0500

    Alert messages added to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  var char = prompt("Password length must be between 8-128 characters.");
+  var lowerCase = confirm("Do you want lowercase letters?");
+  var upperCase = confirm("Do you want uppercase letters?");
+  var numeric = confirm("Do you want numbers?");
+  var specialChar = confirm("Do you want special characters?");
+  
 
 }
 
