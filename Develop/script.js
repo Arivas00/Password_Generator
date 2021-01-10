1ffb3e35d6a9b884e7a95393015669bd3bbdcae1
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-01-10 12:27:16 -0500

    Added array variables for password prompts

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 
+var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k,", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialCharArr = ["!", "#", "$", "%", "&", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "^", "_", "{", "|", "}", "~", "\u0020", "\u0022", "\u0027", "\u0028", "\u0029", "\u002f", "\u005b", "\u005c", "\u005d", "\u0060"];
+
 
 // Write password to the #password input
 function writePassword() {
